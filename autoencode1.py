8784f082a2bc07d7929eac1374444d63cfaa2c56
lulinlin <1710684729@qq.com>
2018-04-13 11:38:58 +0800

    Signed-off-by: lulinlin <1710684729@qq.com>

---
+++ autoencode1.py
@@ -0,0 +1,128 @@
+from __future__ import division, print_function, absolute_import
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 下载并导入MNIST数据集
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+
+# 参数
+learning_rate = 0.01#学习率
+training_epochs = 20#训练的周期
+batch_size = 256#每一批次训练的大小
+display_step = 1
+examples_to_show = 10
+
+# 神经网络的参数
+n_hidden_1 = 784*2# 隐层1的神经元个数
+n_input = 784 # MNIST数据集的输出(img shape: 28*28)
+n_output=10
+
+# tf Graph input (only pictures)
+X = tf.placeholder("float", [None, n_input])
+Y=tf.placeholder('float',[None,10])
+
+weights = {
+    'encoder_h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+    'decoder_h1': tf.Variable(tf.random_normal([n_hidden_1, n_input])),
+    'softmax_w': tf.Variable(tf.random_normal([n_hidden_1, n_output])),
+}
+biases = {
+    'encoder_b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'decoder_b1': tf.Variable(tf.random_normal([n_input])),
+    'softmax_b': tf.Variable(tf.random_normal([n_output])),
+}
+
+# Building the encoder
+def encoder(x):
+    # Encoder Hidden layer with sigmoid activation #1
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']),
+                                   biases['encoder_b1']))
+    # Encoder Hidden layer with sigmoid activation #2
+    return layer_1
+
+
+# Building the decoder
+def decoder(x):
+    # Decoder Hidden layer with sigmoid activation #1
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']),
+                                   biases['decoder_b1']))
+    # Decoder Hidden layer with sigmoid activation #2
+
+    return layer_1
+
+# Construct model
+P=0.1
+beta=1
+encoder_op = encoder(X)
+decoder_op = decoder(encoder_op)
+
+Pj=tf.reduce_mean(encoder_op,0)
+sparse_cost=tf.reduce_mean(P*tf.log(P/Pj)+(1-P)*tf.log((1-P)/(1-Pj)))
+
+# Prediction
+y_pred = decoder_op
+# Targets (Labels) are the input data.
+y_true = X
+
+# Define loss and optimizer, minimize the squared error
+cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))+sparse_cost*beta
+optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+sess = tf.InteractiveSession()
+sess.run(init)
+
+total_batch = int(mnist.train.num_examples/batch_size)
+# Training cycle
+for epoch in range(training_epochs):
+    # Loop over all batches
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        # Run optimization op (backprop) and cost op (to get loss value)
+        _, c = sess.run([optimizer, cost], feed_dict={X: batch_xs})
+    # Display logs per epoch step
+    if epoch % display_step == 0:
+        print("Epoch:", '%04d' % (epoch+1),
+              "cost=", "{:.9f}".format(c))
+
+print("Optimization Finished!")
+
+# Applying encode and decode over test set
+encode_decode = sess.run(
+    y_pred, feed_dict={X: mnist.test.images[:examples_to_show]})
+# Compare original images with their reconstructions
+f, a = plt.subplots(2, 10, figsize=(10, 2))
+for i in range(examples_to_show):
+    a[0][i].imshow(np.reshape(mnist.test.images[i], (28, 28)))
+    a[1][i].imshow(np.reshape(encode_decode[i], (28, 28)))
+f.show()
+plt.draw()
+
+encoder_op = encoder(X)
+y_pred=tf.nn.softmax(tf.matmul(encoder(X),weights['softmax_w'])+biases['softmax_b'])
+cost = tf.reduce_mean(tf.pow(Y - y_pred, 2))
+optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
+
+init = tf.global_variables_initializer()
+
+sess = tf.InteractiveSession()
+sess.run(init)
+
+total_batch = int(mnist.train.num_examples/batch_size)
+
+for epoch in range(training_epochs):
+    # Loop over all batches
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        # Run optimization op (backprop) and cost op (to get loss value)
+        _, c = sess.run([optimizer, cost], feed_dict={X: batch_xs,Y:batch_ys})
+    # Display logs per epoch step
+    if epoch % display_step == 0:
+        print("Epoch:", '%04d' % (epoch+1),
+              "cost=", "{:.9f}".format(c))
+
+print("Optimization Finished!")
